479e2a65e44cb0e55f39c76239bb4f8514a930bf
Hritcu Serafim <hritcuserafim01@gmail.com>
2025-07-09 20:59:34 +0300

    Add ScrollToTop component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "./ui/ScrollToTop";
 
 const Layout = () => {
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ScrollToTop />
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
